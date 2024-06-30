f1f7dbc4d81e9b0b6a28b41a96bf7529ee0b84fd
asmaaHamdym <asmahamdym@gmail.com>
2024-06-30 19:01:31 +0300

    Added all code from the first session

---
+++ lesson1.py
@@ -0,0 +1,37 @@
+# print to the screen
+print("Hello World")
+
+# Arithmatic operators
+print(2 + 7)
+print(5 - 2)
+print(3 * 5)
+print(9 / 3)
+print(7 % 2)
+print(7 ** 2)
+
+# declare a variable of number data type
+# the = sign means we assign the value on the left to the variable on the right
+age = 34
+
+# declare a variable with string data type
+name = 'Mohamed'
+city = 'Cairo'
+
+# string concatination(joining)
+print(name + ' is from ' + city)    
+
+# string formatting using f-string
+print(f'{name} is {age} years old and from {city} ')
+
+#some string methods
+print(name.upper())
+print(name.lower())
+print("mohamed".count('m'))
+print("mohamed".find('m'))
+print("mohamed".replace('m', 'M'))
+print("mohamed".startswith('m'))
+
+
+# comments start with a # sign
+# comments can be used to explain the code or to exclude a line of code
+# print("Hello World") ---> this line is commented out meaning not excuted 
