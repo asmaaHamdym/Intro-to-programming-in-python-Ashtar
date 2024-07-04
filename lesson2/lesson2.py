ce91ef536389f4b96b2a8afb7c7e4f1b873449c3
asmaaHamdym <asmahamdym@gmail.com>
2024-07-04 12:43:36 +0300

    added lesson2 code

--- lesson2/lesson2.py
+++ lesson2/lesson2.py
@@ -7,30 +7,32 @@ print(egp)
 # type function
 print(type(usd))
 print(type(egp))
+#  type conversion
+# float to int (whole number)
 print(int(egp))
-# #  type conversion
-# #int to float             
+#int to float             
 usd = float(usd)
 print(usd)
 print(type(usd))
 
 
 
-# # taking input from the user
+# taking input from the user
 
 age = input("What is your age? ")
 
 print(f'you are {age} years old')
 print(type(age))
 
-# # Budget planner
+# Budget planner
 salary = int(input("What is your salary? "))
 food = int(input("How much do you spend on food? "))
 rent = int(input("How much do you spend on rent? "))
 phone = int(input("How much do you spend on phone? "))
 transportation = int(input("How much do you spend on transportation? "))
-savings =  salary - ( food + rent + phone + transportation)
-print(f'You have {savings} left to save')
+total_expenses = food + rent + phone + transportation
+savings =  salary - total_expenses
+print(f'You have {savings} EGP left to save!')
 # what happens if we typed a string?
 
 # importing modules
@@ -55,6 +57,8 @@ print(random_from_range)
 # print hello 5 times
 for number in range(5):
     print('hello')
+print("Out of the loop")
+
 
 for character in 'Banana':
     print(character)        
@@ -67,19 +71,22 @@ for number in range(1, 30, 3):
 # while loop:
 
 # print hello 5 times
-number = 5
-while number > 0:
+count = 5
+while count > 0:
     print('hello')  
-    number = number - 1
-
+    print(count)
+    count = count - 1
+# -------------------------------
 
 # Elevator example
 elevator_capacity = 5
 while elevator_capacity > 0 :
     print('Please come in')
     elevator_capacity = elevator_capacity - 1
+    print(f"{elvator_capacity} spaces left")
 
 print('Elevator is full')
+# ------------------------------
     
 # functions
 #hello name function
