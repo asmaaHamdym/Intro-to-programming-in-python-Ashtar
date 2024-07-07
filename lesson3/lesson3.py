eb192060a63b0e65aedae9013a32aa9d54357935
asmaaHamdym <asmahamdym@gmail.com>
2024-07-07 07:57:49 +0300

    added lesson3

---
+++ lesson3/lesson3.py
@@ -0,0 +1,101 @@
+# Starter: Rewrite this code to use a for loop and the range() function:
+# print('~' * 0)
+# print('~' * 1)
+# print('~' * 2)
+# print('~' * 3)
+# print('~' * 4)
+# print('~' * 5)
+# print('~' * 6)
+# print('~' * 7)
+# print('~' * 8)
+
+# for num  in range(9):
+#     print('~' * num)
+
+# Boolean data type
+
+# is_weather_hot = True
+# print(type(is_weather_hot))
+
+# Comparison operators
+# ==, !=, >, <, >=, <=
+# print( 10 == (2+5))
+# print(5 != 5)
+# print( 10 > 6)3
+# print( 3 < 2)
+# print( 3 >= 3)
+# print( 5 <= 5)
+
+
+# today = input("What day is it? ")
+# is_Monday = (today == 'Monday')
+# print(is_Monday)
+
+# How much is the sandwitch?
+# price =  input("How much is the sandwitch? ")
+# is_within_budget = float(price) <= 10
+# print(f"Sandwitch is within budget: {is_within_budget}")
+
+# Logical operators
+# and, or, not
+# is_sunny = True
+# is_too_hot = True
+
+# # and - not 
+# is_good_weather = is_sunny and not is_too_hot
+# print(f"Is good weather: {is_good_weather}")
+# # or 
+# is_good_weather = is_sunny or not is_too_hot
+# print(f"Is good weather: {is_good_weather}")
+
+# # not 
+# is_good_weather = not is_sunny and not is_too_hot
+# print(f"Is good weather: {is_good_weather}")
+
+# If statment
+
+
+# name = input("What is your name? ")
+# is_admin = name == "admin"
+# password = input("What is your password? ")
+# is_valid_password = password == "abcdefg"
+
+# if is_admin and is_valid_password:
+#     print("Welcome admin")
+# if not is_admin or not is_valid_password:
+#     print("Access denied")
+
+# Rewite it using else
+
+# password = input("Enter Password: ")
+# confirm_password = input("Enter Password again: ")
+
+# if len(password) >= 8 and password.isalnum():
+#     print("Password is valid")
+
+# if len(confirm_password) >= 8 and confirm_password.isalnum():
+#     print("Password is valid")
+
+# if password == confirm_password:
+#     print("Passwords match")
+
+# Rewite it using else
+
+# rewrite the sandwitch example using if statments
+
+# Books or Movies example
+choice = input("Books or Movies? ")
+
+if choice == 'books':
+    print("📖")
+else:
+    print("🍿")
+
+
+
+# Random number game
+
+
+
+
+
