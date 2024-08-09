da6f0b0dd3540a918514277020cc0593313e9554
asmaaHamdym <asmahamdym@gmail.com>
2024-08-09 13:53:31 +0300

    Replaced hardcoded API key with 'YOUR_API_KEY'

--- lesson6/lessson6.py
+++ lesson6/lessson6.py
@@ -4,7 +4,7 @@ import requests
 
 
 def get_temp(city):
-    API_key = 'e47a270fe3a33a7fe2f42131261ecbea'
+    API_key = 'YOUR_API_KEY'
 
 
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}'
@@ -27,13 +27,6 @@ def main():
     raw_temp = get_temp(city)
     temp = convert_temp(raw_temp)
     print(f'Current temperature in {city} is: {temp} 🌤️')
-
-
-
-
-
-
-
 
 
 
